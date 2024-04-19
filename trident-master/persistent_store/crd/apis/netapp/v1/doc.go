f21dfb80f4cbeef240f1341438e1e89c1f7ae1d3
Yaron Pery <ypery@ypery-thinkpadx1carbongen9.raanana.csb>
2024-04-19 12:39:16 +0300

    delete trident-master

--- trident-master/persistent_store/crd/apis/netapp/v1/doc.go
+++
@@ -1,7 +0,0 @@
-// Copyright 2019 NetApp, Inc. All Rights Reserved.
-
-// +k8s:deepcopy-gen=package
-// +groupName=trident.netapp.io
-
-// This package has the automatically generated clientset.
-package v1
